c1c445c23506a8f5c38227cedca7e1f99bef7291
Owen Carter <owenc@playroll.com>
2024-09-15 10:50:23 +0100

    marrying e2e with documentation

--- tests/security-tests.spec.ts
+++ tests/security-tests.spec.ts
@@ -4,6 +4,8 @@ import { assertFieldSafeFromXss } from '../src/module-tests/popup';
 test.beforeEach(async ({ page, extensionId }) => page.goto(`chrome-extension://${extensionId}/popup.html`));
 
 // This test has been made to fail by design.
-test('Should handle XSS input safely', async ({ page }) => {
-  expect(await assertFieldSafeFromXss(page)).toBe(true);
+test.describe('TC-005 Check For Security Vulnerabilities', () => {
+  test('Should handle XSS input safely', async ({ page }) => {
+    expect(await assertFieldSafeFromXss(page)).toBe(true);
+  });
 });
